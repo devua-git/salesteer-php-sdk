77d6f60ce52760857f32c82c2382498d87725e29
Alessandro Lodi <alelod96@gmail.com>
2023-08-21 11:49:49 +0200

    first commit

---
+++ lib/Exception/ExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Salesteer\Exception;
+
+use Throwable;
+
+interface ExceptionInterface extends Throwable
+{
+}
---
+++ lib/WebhookSignature.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Salesteer;
+
+class WebhookSignature
+{
+    public static function verifyHeader($payload, $secret,  $tolerance = null)
+    {
+        $signature = $_SERVER['HTTP_X_SALESTEER_HMAC_SHA256'];
+        $timestamp = $_SERVER['HTTP_X_SALESTEER_TIMESTAMP'];
+
+        $signedPayload = "{$timestamp}.{$payload}";
+
+        $expectedSignature = self::computeSignature($signedPayload, $secret);
+
+        if (!hash_equals($expectedSignature, $signature)) {
+            throw Exception\SignatureVerificationException::factory(
+                'No signatures found matching the expected signature for payload', $payload, $signature
+            );
+        }
+
+        // Check if timestamp is within tolerance
+        if (($tolerance > 0) && (abs(time() - $timestamp) > $tolerance)) {
+            throw Exception\SignatureVerificationException::factory(
+                'Timestamp outside the tolerance zone', $payload, $signature
+            );
+        }
+
+        return true;
+    }
+
+    private static function computeSignature($payload, $secret)
+    {
+        return hash_hmac('sha256', $payload, $secret);
+    }
+}
