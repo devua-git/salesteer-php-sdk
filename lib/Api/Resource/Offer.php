d65aa2b5786df3104893a652149071ead2812482
Mario Catillo <m.catillo@devua.it>
2023-08-25 12:56:42 +0200

    offerproducts

--- lib/Api/Resource/Offer.php
+++ lib/Api/Resource/Offer.php
@@ -11,4 +11,16 @@ class Offer extends ApiResource
     use ApiOperation\Drop;
 
     const OBJECT_NAME = 'offer';
+
+    const RELATION_TO_CLASS = [
+        'products' => OfferProduct::class
+    ];
+
+
+    public function saveProducts(array $products = null)
+    {
+        $url = $this->instanceUrl() . '/products';
+        $res = $this->request('post', $url, $products, null, Offer::class);
+        $this->refreshFrom($res);
+    }
 }
